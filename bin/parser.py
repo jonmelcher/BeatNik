bf296cb6c0dc8a4c7281038a2cc85061b1764c61
Jonathan <jonathan.melcher@gmail.com>
2014-09-09 22:28:00 -0600

    Cleaned up
    Cleaned up, added documentation, and added examples.

--- bin/parser.py
+++ bin/parser.py
@@ -1,22 +1,26 @@
 """
-This program will access a search result page from
-http://www.bpmdatabase.com and parse all song data
-from the page source.
+This file contains files serving to scrape and parse data
+found on online databases.
 
-Some examples:
-http://www.bpmdatabase.com/search.php?artist=radiohead&title=&mix=&bpm=&gid=&label=&year=&srt=artist&ord=desc
+Current databases:
 
-Notes:
-Crude but seems to work for this website.  Let me know what you think.
-One problem will be multiple word bandnames/albums/songnames as they
-get split up by this program into different list entries.
+http://www.BPMdatabase.com
+    -BPMdatabaseParse   : url ->  [[Song Data]]
+    -BPMdatabaseBandGrab: bandname -> [[Songs]]
+
+Newest Changes:
+-Added this, please modify below! -J 9/9/14
+
+-Fixed bug where BandGrab would continue onto next bandname due to
+ manual database access(?).  Replacing spaces with '+' in bandname
+ seems to have worked. -J 9/9/14
 """
 
 import urllib2
 import re
+#import classes (Song)
 
-
-""" This class should be in another file."""
+#This class should be in another file.
 class Song(object):
     'A song.'
 
@@ -25,16 +29,18 @@ class Song(object):
         self.data   = data
     def __repr__(self):
         return repr(self.data)
-""""""
+        
 
 def BPMdatabaseParse(url):
-    'Parses all song data on a BPMdatabase.com search page into an array'
+    'Parses all song data from a page on http://www.BPMdatabase.com.'
+    'Type: String -> [[String]]'
+    'Each entry corresponds to Song initialization data.'
 
     page = urllib2.urlopen(url)
-
-    # find the line containing the table of search results
     text = []
-    for line in page:
+    
+    # Finds the line containing the table of search results.
+    for line in page:   
         if re.findall('<tr class="line2".*tr>', line):
             text = re.findall('<tr class="line2".*tr>', line)[0]
             break
@@ -42,12 +48,12 @@ def BPMdatabaseParse(url):
     if not text or 'No records found.' in text:
         return None
 
-    # split up the search table by row
+    # Splits up the search results table by row.
     temp_data = re.split('<tr class="line[12]">', text)
     temp_data.remove('')
 
-    # collect the column entries for each row
-    # filled replaces empty columns with 'NULL'
+    # Collects the column entries from each row.
+    # Replaces empty column entries with 'NULL'.
     data = []
     for d in temp_data:
         filled   = re.sub('<td></td>','<td>NULL</td>', d)
@@ -58,20 +64,23 @@ def BPMdatabaseParse(url):
 
 
 def BPMdatabaseBandGrab(bandname):
-    'Grabs all songs by the band in question.'
-    'Stores them in a list of Song instances.'
+    'Grabs and parses all songs by the band in question'
+    'from http://www.BPMdatabase.com.'
+    'Type: String -> [Song]'
+    'Each Song is initialized from data returned from'
+    'BPMdatabaseParse.'
 
-    songs   = []
-    begin   = ['?begin=', 0]
-    num     = ['&num='  , 1]
-    rooturl = "http://www.bpmdatabase.com/search.php"
+    bandname = bandname.rstrip().replace(' ','+')
+    songs    = []
+    begin    = ['?begin=', 0]
+    num      = ['&num='  , 1]
+    rooturl  = "http://www.bpmdatabase.com/search.php"
 
-    # might be good way to write this, this is quite ugly
+    # If you can find a better way to represent the variable URL use it.
     scraped_data = BPMdatabaseParse(rooturl + (''.join(map(str,begin))
           + ''.join(map(str,num)) + "&numBegin=0&artist=" + bandname))
 
     while scraped_data:
-
         songs       += [Song(x) for x in scraped_data]
         begin[1]    += 10
         num[1]      += 1
@@ -83,6 +92,21 @@ def BPMdatabaseBandGrab(bandname):
 
 if __name__ == '__main__':
 
-    example = "http://www.bpmdatabase.com/search.php?begin=0&num=1&numBegin=0&artist=tragically+hip"
-    data = BPMdatabaseParse(example)
-    print data
+    example1 = "http://www.bpmdatabase.com/search.php?begin=0&num=1&numBegin=0&artist=tragically+hip"
+    example2 = "http://www.bpmdatabase.com/search.php?begin=0&num=1&numBegin=0&artist=tragically Hip"
+    example3 = "http://www.bpmdatabase.com/search.php?begin=0&num=1&numBegin=0&artist=Radiohead"
+    example4 = "Radiohead"
+    example5 = "radiOhead"
+    example6 = "Avril Lavigne"
+    example7 = "Sum+41"
+
+    print "Example Results for BPMdatabaseParse:"
+    print "url for 'tragically+hip', 'tragically Hip', and 'Radiohead'."
+    print BPMdatabaseParse(example1)
+    print BPMdatabaseParse(example2)
+    print BPMdatabaseParse(example3)
+    print "Example Results for BPMdatabaseBandGrab:"
+    print "Search terms 'Radiohead', 'radiOhead', 'Avril Lavigne', and 'Sum+41'."
+    print BPMdatabaseBandGrab(example1)
+    print BPMdatabaseBandGrab(example2)
+    print BPMdatabaseBandGrab(example3)
