13d28fa1c5bcf0f10f3ef75dfa239c6bad7b520b
jmelcher86 <jonathan.melcher@gmail.com>
2014-09-09 20:36:34 -0600

    Fixed Parser

--- bin/parser.py
+++ bin/parser.py
@@ -16,36 +16,74 @@ import urllib2
 import re
 
 
+""" This class should be in another file."""
+class Song(object):
+    'A song.'
 
-def BPMdatabaseparse(url):
+    def __init__(self, data):
+
+        self.data   = data
+        
+    def __repr__(self):
+        return repr(self.data)
+""""""
+
+def BPMdatabaseParse(url):
     'Parses all song data on a BPMdatabase.com search page into an array'
 
     page = urllib2.urlopen(url)
 
     # find the line containing the table of search results
+    text = []
     for line in page:
-        text = re.findall('<tr class="line2".*tr>', line)[0]
         if re.findall('<tr class="line2".*tr>', line):
+            text = re.findall('<tr class="line2".*tr>', line)[0]
             break
+            
+    if not text or 'No records found.' in text:
+        return None
 
     # split up the search table by row
-    temp_data = re.split('<tr class', text)
+    temp_data = re.split('<tr class="line[12]">', text)
     temp_data.remove('')
 
     # collect the column entries for each row
+    # filled replaces empty columns with 'NULL'
     data = []
     for d in temp_data:
-        new_data = re.findall('<td>([\w ]+?)</td>', d)
+        filled   = re.sub('<td></td>','<td>NULL</td>', d)
+        new_data = re.findall('<td>([\w ]+?)</td>', filled)
         data.append(new_data)
-
 
     return data
 
 
-if __name__ == '__main__':
-    ls = ["http://www.bpmdatabase.com/","search.php?artist=radiohead",
-        "&title=&mix=&bpm=&gid=","&label=&year=&srt=artist&ord=desc"]
+def BPMdatabaseBandGrab(bandname):
+    'Grabs all songs by the band in question.'
+    'Stores them in a list of Song instances.'
 
-    example = ''.join(ls)
-    data = BPMdatabaseparse(example)
+    songs   = []
+    begin   = ['?begin=', 0]
+    num     = ['&num='  , 1]
+    rooturl = "http://www.bpmdatabase.com/search.php"
+
+    # might be good way to write this, this is quite ugly
+    scraped_data = BPMdatabaseParse(rooturl + (''.join(map(str,begin))
+          + ''.join(map(str,num)) + "&numBegin=0&artist=" + bandname))
+
+    while scraped_data:
+
+        songs       += [Song(x) for x in scraped_data]
+        begin[1]    += 10
+        num[1]      += 1
+        scraped_data = BPMdatabaseParse(rooturl + (''.join(map(str,begin))
+              + ''.join(map(str,num)) + "&numBegin=0&artist=" + bandname))
+
+    return songs
+
+
+if __name__ == '__main__':
+
+    example = "http://www.bpmdatabase.com/search.php?begin=0&num=1&numBegin=0&artist=tragically+hip"
+    data = BPMdatabaseParse(example)
     print data
