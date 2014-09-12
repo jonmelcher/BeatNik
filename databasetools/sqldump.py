842fd0cb6b3237e8ad5f8174c6c2320fd878a032
kaptainkayak <kaptainkayak@gmail.com>
2014-09-12 19:54:54 -0400

    song_db class probably works, but it's untested

--- databasetools/sqldump.py
+++ databasetools/sqldump.py
@@ -3,6 +3,7 @@ An example of how to add to a database.
 '''
 
 import sqlite3
+from Classes import Song
 
 class song_db(object):
     'Initializes and manipulates a database of songs'
@@ -23,13 +24,31 @@ class song_db(object):
         self.cursor = self.conn.cursor()
 
     def create_table(self, name):
-
-        self.cursor.execute('''CREATE TABLE %s
+        try:
+            self.cursor.execute('''CREATE TABLE %s
                 (title text, artist text, album text, label text, year int, genre text, BPM real)''' % name)
+        except sqlite3.OperationalError:
+            print 'The table %s already exists' % name
+
+    def delete_table(self, name):
+        self.cursor.executescript('drop table if exists %s;' % name)
 
 
-    def insert_song(self, song):
-        print "I'm too lazy."
+    def insert_song(self, song, table):
+
+
+        self.cursor.execute('''INSERT INTO %s VALUES
+            ('%s', '%s', '%s', '%s', '%s', '%s', '%s')'''
+            % (table,
+               song.artist,
+               song.title,
+               song.album,
+               song.BPM,
+               song.genre,
+               song.label,
+               song.year,
+              )
+            )
 
     def delete_song(self, song):
         print "No."
@@ -43,7 +62,29 @@ class song_db(object):
 
 if __name__ == '__main__':
     db = song_db('BPMdatabase', 'bpmdatabase.db')
-    db.insert_song('hello')
+    table_name = 'my_songs'
+    db.delete_table(table_name)
+    db.create_table(table_name)
+
+    my_song_data = ['Radiohead', 'Time of my life', 'The Very Best of Neil Young', 9001, 'Electronic', 'Spam Recording Inc', 2015]
+
+    for i, d in enumerate(my_song_data):
+        if isinstance(d, str):
+           my_song_data[i] = d.replace(' ', '')
+    my_song = Song(my_song_data)
+
+    # print my_song.artist, my_song.title, my_song.label, my_song.year
+
+    db.insert_song(my_song, table_name)
+
+
+        # self.artist   = song_data[0]
+        # self.title    = song_data[1]
+        # self.album    = song_data[2]
+        # self.BPM      = song_data[3]
+        # self.genre    = song_data[4]
+        # self.label    = song_data[5]
+        # self.year     = song_data[6]
 
 
 # conn = sqlite3.connect('bpmdatabase.db')
