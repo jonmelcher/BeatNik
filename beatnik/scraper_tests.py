7781c6112fc978e06eb4214b6b6e84784de50353
jmelcher86 <jonathan.melcher@gmail.com>
2014-09-13 17:59:06 -0600

    Automated BPMD.raw_table_grab testing

--- beatnik/scraper_tests.py
+++ beatnik/scraper_tests.py
@@ -27,6 +27,7 @@ example_0 = (
             '<tr class="line2"><td>1</td><td>1</td><td></td><td>1 BPM</td><td></td><td></td><td></td></tr><tr class="line1"><td>1</td><td>1</td><td></td><td>1 BPM</td><td></td><td></td><td></td></tr><tr class="line2"><td>1</td><td>1</td><td></td><td>1 BPM</td><td></td><td></td><td></td></tr><tr class="line1"><td>1</td><td>1</td><td>1</td><td>1 BPM</td><td>Acid Techno</td><td>1</td><td>1</td></tr><tr class="line2"><td>1</td><td>-1\'</td><td>1</td><td>1 BPM</td><td>Acid Techno</td><td>1</td><td>1</td></tr><tr class="line1"><td>1</td><td>1</td><td>-1\'</td><td>1 BPM</td><td>Acid Techno</td><td>1</td><td>1</td></tr>',
             [['1', '1', '', '1 BPM', '', '', '', ''], ['1', '1', '', '1 BPM', '', '', '', ''], ['1', '1', '', '1 BPM', '', '', '', ''], ['1', '1', '1', '1 BPM', 'Acid Techno', '1', '1', ''], ['1', "-1'", '1', '1 BPM', 'Acid Techno', '1', '1', ''], ['1', '1', "-1'", '1 BPM', 'Acid Techno', '1', '1', '']]
             )
+
 BPMDB_raw_positive = [example_0]
 
 
@@ -59,19 +60,61 @@ def test_BPMD_raw_table_grab(raw_table_positive, is_large):
     if scrapers.BPMDB.raw_table_grab("", to_test) == raw_table_positive[1]:
         return True
     else:
-        return False
+        return (False, to_test)
 
-def test_BPMD_song_parse(song_parse_positive, is_large):
+def test_BPMD_song_parse(BPMDB_raw_positive, is_large):
 
-    insertion_string = song_parse_positive[0]
+    insertion_string = BPMDB_raw_positive[0]
 
     if is_large:
         to_test = test_string(True, insertion_string)
     else:
         to_test = test_string(False, insertion_string)
 
-    if scrapers.BPMDB.song_parse("", to_test) == song_parse_positive[2]:
+    if scrapers.BPMDB.song_parse("", to_test) == BPMDB_raw_positive[2]:
         return True
     else:
-        return False
+        return (False, to_test)
 
+def automatic_test_BPMD_raw_table_grab(trials):
+
+    small_success = 0
+    large_success = 0
+    print "Automatic testing for BPMD.raw_table_grab using"
+    print "large and small random strings with positive"
+    print "regex matches inserted inside.\n"
+    print "Now testing small random strings..."
+
+    for i in xrange(trials):
+        test_outcome = test_BPMD_song_parse(random.choice(BPMDB_raw_positive), False)
+        if test_outcome == True:
+            small_success += 1
+        else:
+            pass
+            #print "Function failed to properly output %s" % (test_outcome[1])
+
+    print "BPMD.raw_table_grab deals with small strings with"
+    print "A %s efficiency." % (small_success*1.0/trials)
+    print "Now testing large random strings..."
+
+    for i in xrange(trials):
+        test_outcome = test_BPMD_song_parse(random.choice(BPMDB_raw_positive), True)
+        if test_outcome == True:
+            large_success += 1
+        else:
+            pass
+            #print "Function failed to properly output %s" % (test_outcome[1])
+
+    print "BPMD.raw_table_grab deals with large strings with"
+    print "A %s efficiency." % (large_success*1.0/trials)
+
+    if 0.9 < small_success*1.0/trials:
+        print "Function has passed the small random strings test."
+        return True
+
+    if 0.9 < large_success*1.0/trials:
+        print "Function has passed the large random strings test."
+        return True
+
+    else:
+        return False
