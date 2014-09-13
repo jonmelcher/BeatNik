f2a229f1e1ffab39a0d8024d28be1226379842c3
jmelcher86 <jonathan.melcher@gmail.com>
2014-09-12 21:42:49 -0600

    Update scrapers.py and Add music.py

--- bin/scrapers.py
+++ bin/scrapers.py
@@ -1,91 +1,17 @@
 """
-This file contains the scrapers for the BeatNik project, as well as the
-Song class.  An example for a song instance:
-Song( ['The Click Five', 'Jenny', 'NULL', '96.00', 'Null', 'Null', 'Null', '3B / Db'] )
+This file contains the scrapers for the BeatNik project.
+Please see BeatNik/documentation.txt for further information
+regarding this file.
 
 Newest Changes:
+    -Added documentation.txt descriptions for scraper.py -J 9/12/14
     -Added AudioKC class, needs to be tested more diligently. -J 9/11/14
-    -Changed style to be more pythonic.
-    -Copied BPMBD class over from parser.py, removed Class module depen-
-     dencies. -J 9/11/14
-    -Created file - J 9/9/14
 """
 import urllib2
 import re
 import time
 
-
-class Song(object):
-    'Song Class is for formatting and maintaining song data.'
-
-    def __init__(self, song_data):
-
-        self.data     = song_data
-        self.artist   = None
-        self.title    = None
-        self.album    = None
-        self.BPM      = None
-        self.genre    = None
-        self.label    = None
-        self.year     = None
-        self.key      = None
-
-
-    def refresh(self):
-        'Method for assigning and renewing data. Allows data editing to'
-        'be done solely with self.data.'
-        self.artist = self.data[0]
-        self.title  = self.data[1]
-        self.album  = self.data[2]
-        self.BPM    = self.data[3]
-        self.genre  = self.data[4]
-        self.label  = self.data[5]
-        self.year   = self.data[6]
-        self.key    = self.data[7]
-
-
-    def change_to(self, index, new_value):
-        'Method for changing song data.'
-        try:
-            self.data[index] = new_value
-        except Exception as e:
-            raise IndexError('Index must be values 0-%s' % len(self.data))
-        return self.refresh()
-
-
-    def __add__(self, other):
-        'Method for merging songs together. data[i] must be identical'
-        'or one \'Null\' to merge.'
-        'Type: Song -> Song -> Song'
-        merged_song_data = []
-        for i, element in enumerate(self.data):
-            if element == other.data[i]:
-                merged_song_data.append(element)
-            elif element == 'Null':
-                merged_song_data.append(other.data[i])
-            elif other.data[i] == 'Null':
-                merged_song_data.append(element)
-            else:
-                raise ValueError('.data must match other than Null values.')
-
-        return Song(merged_song_data)
-
-
-    def __eq__(self, other):
-        'Method for determining equality of songs.'
-        'Type: Song -> Song -> Bool'
-        if self.data[:2] == other.data[:2]:
-            return True
-        return False
-
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-
-    def __repr__(self):
-        return repr(self.data)
-
+import music
 
 
 class BPMDB(object):
@@ -182,7 +108,7 @@ class BPMDB(object):
         scraped_data = BPMDB.song_parse(url)
 
         while scraped_data:
-            songs       += [Song(x) for x in scraped_data]
+            songs       += [music.Song(x) for x in scraped_data]
             begin       += 10
             url          = BPMDB.url_helper(begin, True, bandname)
             scraped_data = BPMDB.song_parse(url)
@@ -301,7 +227,7 @@ class AudioKC(object):
             formatted_song_data = [song_data[1], song_data[0], 'NULL',
                                    song_data[3], 'Null'      , 'Null',
                                    'Null'      , song_data[2]]
-            songs.append(Song(formatted_song_data))
+            songs.append(music.Song(formatted_song_data))
 
         return songs
 
